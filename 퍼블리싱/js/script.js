26ce8faf1d9099a825a51120cbe9afc7d7be1046
이승용 <tmddyd133@naver.com>
2020-11-15 22:17:16 +0900

    first push

---
+++ 퍼블리싱/js/script.js
@@ -0,0 +1,123 @@
+jQuery(document).ready(function(){
+
+//
+	$('').hover(
+		function(){
+			
+		},
+		function(){
+
+		}
+	);
+	
+//
+	$('.content_03 .left .slide_wrap .slide li p.img span').click(function(){
+		var img = $(this).parents('p.img').css("background-image");
+
+		$('.content_03 .left .slide_wrap .slide li').removeClass('on');
+		$(this).parents('li').addClass('on');
+		$('.content_03 .right .img').css('background-image',img);
+	});
+
+	$('#slider_01').slick({
+		slide: 'div',		//슬라이드 되어야 할 태그 ex) div, li 
+		infinite : false, 	//무한 반복 옵션	 
+		slidesToShow : 1,		// 한 화면에 보여질 컨텐츠 개수
+		slidesToScroll : 1,		//스크롤 한번에 움직일 컨텐츠 개수
+		speed : 500,	 // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
+		arrows : true, 		// 옆으로 이동하는 화살표 표시 여부
+		autoplay : true,			// 자동 스크롤 사용 여부
+		autoplaySpeed : 2500, 		// 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
+		pauseOnHover : true,		// 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
+		prevArrow : $('.content_01 .left_arrow'),		// 이전 화살표 모양 설정
+		nextArrow : $('.content_01 .right_arrow'),		// 다음 화살표 모양 설정
+		draggable : true, 	//드래그 가능 여부 
+		
+		responsive: [ // 반응형 웹 구현 옵션
+			{  
+				breakpoint: 960, //화면 사이즈 960px
+				settings: {
+					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+					slidesToShow:3 
+				} 
+			},
+			{ 
+				breakpoint: 768, //화면 사이즈 768px
+				settings: {	
+					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+					slidesToShow:2 
+				} 
+			}
+		]
+
+	});
+	$('#slider_02').slick({
+		slide: 'ul',		//슬라이드 되어야 할 태그 ex) div, li 
+		infinite : false, 	//무한 반복 옵션	 
+		slidesToShow : 1,		// 한 화면에 보여질 컨텐츠 개수
+		slidesToScroll : 1,		//스크롤 한번에 움직일 컨텐츠 개수
+		speed : 500,	 // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
+		arrows : false, 		// 옆으로 이동하는 화살표 표시 여부
+		dots : true, 		// 스크롤바 아래 점으로 페이지네이션 여부
+		autoplay : true,			// 자동 스크롤 사용 여부
+		autoplaySpeed : 3500, 		// 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
+		pauseOnHover : true,		// 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
+		draggable : true, 	//드래그 가능 여부 
+		
+		responsive: [ // 반응형 웹 구현 옵션
+			{  
+				breakpoint: 960, //화면 사이즈 960px
+				settings: {
+					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+					slidesToShow:3 
+				} 
+			},
+			{ 
+				breakpoint: 768, //화면 사이즈 768px
+				settings: {	
+					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+					slidesToShow:2 
+				} 
+			}
+		]
+
+	});
+
+	$('#slider_03').slick({
+		slide: 'ul',		//슬라이드 되어야 할 태그 ex) div, li 
+		infinite : false, 	//무한 반복 옵션	 
+		slidesToShow : 1,		// 한 화면에 보여질 컨텐츠 개수
+		slidesToScroll : 1,		//스크롤 한번에 움직일 컨텐츠 개수
+		speed : 500,	 // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
+		arrows : true, 		// 옆으로 이동하는 화살표 표시 여부
+		autoplay : true,			// 자동 스크롤 사용 여부
+		autoplaySpeed : 2500, 		// 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
+		pauseOnHover : true,		// 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
+		prevArrow : $('.content_03 .left_arrow'),		// 이전 화살표 모양 설정
+		nextArrow : $('.content_03 .right_arrow'),		// 다음 화살표 모양 설정
+		draggable : true, 	//드래그 가능 여부 
+		
+		responsive: [ // 반응형 웹 구현 옵션
+			{  
+				breakpoint: 960, //화면 사이즈 960px
+				settings: {
+					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+					slidesToShow:3 
+				} 
+			},
+			{ 
+				breakpoint: 768, //화면 사이즈 768px
+				settings: {	
+					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+					slidesToShow:2 
+				} 
+			}
+		]
+
+	});
+	
+});
+
+
+
+
